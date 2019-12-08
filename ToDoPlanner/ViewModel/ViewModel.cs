998496ba848da311e8f9e8e3cd7f00542e4e1afc
Regula <regula.s.engelhardt@gmail.com>
2019-12-08 16:22:32 +0100

    init ViewModelBaseClass

--- ToDoPlanner/ViewModel/ViewModel.cs
+++ ToDoPlanner/ViewModel/ViewModel.cs
@@ -1,41 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using ToDoPlanner.Annotations;
 
 namespace ToDoPlanner.ViewModel
 {
-    public class TaskModel : INotifyPropertyChanged
+    public class TaskModel : ViewModelBase
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+        private string _title;
 
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        private string _description;
+
+        public string Description
+        {
+            get => _description;
+            set => SetProperty(ref _description, value);
+        }
+
         public enum Priority
         {
-            Low, Medium, High
+            Low,
+            Medium,
+            High
         }
+
         public int PriorityNum { get; set; }
-        public DateTime Deadline  { get; set; }
-        public DateTime StartDate  { get; set; }
-        public string Category { get; set; }
+        public DateTime Deadline { get; set; }
+        public DateTime StartDate { get; set; }
+        private string _category;
+
+        public string Category
+        {
+            get => _category;
+            set => SetProperty(ref _category, value);
+        }
+
         public enum Status
         {
-            None, Open, Planned, Ongoing, Done
+            None,
+            Open,
+            Planned,
+            Ongoing,
+            Done
         }
+
         public DateTime Created { get; set; }
         public DateTime Changed { get; set; }
         public int Effort { get; set; }
         public int Progress { get; set; } // Implement as progressbar in the MainWindow.xaml
-
-        [NotifyPropertyChangedInvocator]
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
     }
 }
---
+++ ToDoPlanner/ViewModel/ViewModelBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using ToDoPlanner.Annotations;
+
+namespace ToDoPlanner.ViewModel
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if(!EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                field = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
