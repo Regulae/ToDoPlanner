50df48766cf8f609ec204ae21dd35c3fb149bc9c
bari1337 <57557049+bari1337@users.noreply.github.com>
2019-12-13 18:13:56 +0100

    Added binding proxy

---
+++ ToDoPlanner/Model/BindingProxy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ToDoPlanner.Model
+{
+    public class BindingProxy : Freezable
+    {
+        public static readonly DependencyProperty DataProperty =
+           DependencyProperty.Register("Data", typeof(object),
+              typeof(BindingProxy));
+
+        public object Data
+        {
+            get { return GetValue(DataProperty); }
+            set { SetValue(DataProperty, value); }
+        }
+
+        #region Overrides of Freezable
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new BindingProxy();
+        }
+
+        #endregion
+    }
+}
