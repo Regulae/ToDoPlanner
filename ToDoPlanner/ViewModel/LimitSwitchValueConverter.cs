2c2ede471d8e2f83af406c8202265fbab86eda99
Kevin Kessler <kevin_kessler@msn.com>
2020-01-11 23:09:12 +0100

    Added converter for progressbar and auto design alignment

---
+++ ToDoPlanner/ViewModel/LimitSwitchValueConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ToDoPlanner.ViewModel
+{
+    class LimitSwitchValueConverter : IValueConverter
+    {
+
+        /// <summary>
+        /// Add a percentage symbol to the end of an value.
+        /// </summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">A converted value. If the method returns null, the valid null value is used.</param>
+        /// <returns>A string with a percentage symbol in the end.</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string para = parameter as string;
+
+            if(para != null)
+            {
+                string[] parameters = para.Split(',');
+                if(parameters.Length == 3)
+                {
+                    int limit = System.Convert.ToInt32(parameters[0]);
+                    int lowValue = System.Convert.ToInt32(parameters[1]);
+                    int highValue = System.Convert.ToInt32(parameters[2]);
+
+                    return ((limit < System.Convert.ToInt32(value)) ? highValue : lowValue);
+                }
+            }
+            return 1;
+        }
+
+        /// <summary>
+        /// Remove percentage symbols from a the value string and convert to an int
+        /// </summary>
+        /// <param name="value">The value that is produced by the binding target.</param>
+        /// <param name="targetType">The type to convert to.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>An interger without percentage symbol</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ToDoPlanner/ViewModel/PercentageValueConverter.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ToDoPlanner.ViewModel
+{
+    class PercentageValueConverter : IValueConverter
+
+    {
+        /// <summary>
+        /// Add a percentage symbol to the end of a value.
+        /// </summary>
+        /// <param name="value">The value produced by the binding source. Limit from 0 to 100 if no paramters are used.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use. Possible Paramter "lowerLimit,upperLimit", e.g. "-50,250" = limit the value from -50 to 200.</param>
+        /// <param name="culture">A converted value. If the method returns null, the valid null value is used.</param>
+        /// <returns>A string with a percentage symbol in the end. If conversation fails return "0%"</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            long lowerLimit = 0;
+            long upperLimit = 100;
+
+            try
+            {
+                string para = parameter as string;
+
+                if (para != null)
+                {
+                    string[] parameters = para.Split(',');
+                    if (parameters.Length == 2)
+                    {
+                        lowerLimit = System.Convert.ToInt64(parameters[0]);
+                        upperLimit = System.Convert.ToInt64(parameters[1]);
+                    }
+                }
+
+                long tempVal = System.Convert.ToInt64(value);
+                tempVal = (tempVal > upperLimit) ? upperLimit : tempVal;
+                tempVal = (tempVal < lowerLimit) ? lowerLimit : tempVal;
+
+                return System.Convert.ToString(tempVal) + '%';
+            }
+            catch
+            {
+                return "0%";
+            }
+        }
+
+        /// <summary>
+        /// Remove percentage symbols from the value string and convert to an int. Limit from 0 to 100 if no paramters are used.
+        /// </summary>
+        /// <param name="value">The value that is produced by the binding target.</param>
+        /// <param name="targetType">The type to convert to.</param>
+        /// <param name="parameter">The converter parameter to use. Possible Paramter "lowerLimit,upperLimit", e.g. "-50,250" = limit the value from -50 to 200.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>An interger without percentage symbol. If conversation fails return 0</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            long lowerLimit = 0;
+            long upperLimit = 100;
+
+
+            try
+            {
+                string para = parameter as string;
+
+                if (para != null)
+                {
+                    string[] parameters = para.Split(',');
+                    if (parameters.Length == 2)
+                    {
+                        lowerLimit = System.Convert.ToInt64(parameters[0]);
+                        upperLimit = System.Convert.ToInt64(parameters[1]);
+                    }
+                }
+
+                long tempVal = System.Convert.ToInt64(((string)value).Trim('%'));
+                tempVal = (tempVal > upperLimit) ? upperLimit : tempVal;
+                tempVal = (tempVal < lowerLimit) ? lowerLimit : tempVal;
+
+                return tempVal;
+            }
+            catch
+            {
+                return 0;
+            }
+
+        }
+    }
+}
