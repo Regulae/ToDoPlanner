fc6704f76b7bee493a7d3869bd6c30b8844e445c
Kevin Kessler <kevin_kessler@msn.com>
2020-01-20 22:07:38 +0100

    Added StringToDateTimeConverter

---
+++ ToDoPlanner/ViewModel/StringToDateTimeConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ToDoPlanner.ViewModel
+{
+    public class StringToDateTimeConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+            return ((DateTime)value).ToString(parameter as string, CultureInfo.InvariantCulture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (string.IsNullOrEmpty(value as string))
+            {
+                return null;
+            }
+            try
+            {
+                DateTime dt = DateTime.ParseExact(value as string, parameter as string, CultureInfo.InvariantCulture);
+                return dt as DateTime?;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
